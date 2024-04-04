438a0478f8532e4c0a67783e272fdd60388104cb
Renato Barucco <barucco@gmail.com>
2024-04-04 00:34:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const fs = require('fs-extra');
 const semver = require('semver');
 const dedent = require('dedent');
 
-/* const express = require('express');
-const app = express();
-const path = require('path');
-
-var PORT = process.env.port || 3001;
-
-const SLDS_DIR = '/node_modules/@salesforce-ux/design-system/assets';
-
-app.use('/slds', express.static(__dirname + SLDS_DIR));
-
-app.get('/', function (req, res) {
-  //res.send('Hello World!');
-  res.sendFile(path.resolve('index.html'));
-});
-app.listen(PORT, function () {
-  console.log(`App listening on port ${PORT}`);
-}); */
-
 const express = require('express');
 const app = express();
 const path = require('path');
